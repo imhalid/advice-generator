06afb892440dfd1627728e19d93f1b6c6bbafeb1
imhalid <imhalid@icloud.com>
2022-06-20 20:08:06 +0300

    finish this project

--- js/script.js
+++ js/script.js
@@ -2,15 +2,19 @@ const advice = document.querySelector("#advice");
 const adviceID = document.querySelector("#adviceID");
 const button = document.querySelector("#generate");
 
-const randomNumber = Math.floor(Math.random() * 223);
-
-const apiURL = `https://api.adviceslip.com/advice/${randomNumber}`;
-
 const getAdvice = async () => {
+  const randomNumber = Math.floor(Math.random() * 223);
+
+  const apiURL = `https://api.adviceslip.com/advice/${randomNumber}`;
+
   const res = await fetch(apiURL);
   const data = await res.json();
   advice.innerHTML = data.slip.advice;
   adviceID.innerHTML = data.slip.id;
 };
+
+button.addEventListener("click", function (e) {
+  getAdvice();
+});
 
 getAdvice();
