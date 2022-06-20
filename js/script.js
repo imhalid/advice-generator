f7de75e1f4202021b320735091531e650e1af690
imhalid <imhalid@icloud.com>
2022-06-20 18:37:54 +0300

    add UI and js function

--- js/script.js
+++ js/script.js
@@ -1,18 +1,14 @@
 const advice = document.querySelector("#advice");
 const adviceID = document.querySelector("#adviceID");
-
-advice.innerHTML = "advice";
+const button = document.querySelector("#generate");
 
 const randomNumber = Math.floor(Math.random() * 223);
+
 const apiURL = `https://api.adviceslip.com/advice/${randomNumber}`;
 
 const getAdvice = async () => {
   const res = await fetch(apiURL);
   const data = await res.json();
-
-  //create random number
-
-  console.log(data);
   advice.innerHTML = data.slip.advice;
   adviceID.innerHTML = data.slip.id;
 };
